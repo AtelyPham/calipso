498afa58877d9d0f76c5b69a29bebf158022995d
AtelyPham <tinpham1412000@gmail.com>
2022-07-03 14:21:39 +0700

    chore: change video url to local repo

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,10 +23,7 @@ const Home = () => {
           loop
           muted
         >
-          <source
-            src='https://nft4charity-assets.s3.us-east-2.amazonaws.com/hero-video.mp4'
-            type='video/mp4'
-          />
+          <source src='/hero-video.mp4' type='video/mp4' />
           Your browser does not support the video tag.
         </video>
 
