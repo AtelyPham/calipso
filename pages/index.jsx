da8f68f538ed65dc71b3122265366a2b7089f594
AtelyPham <tinpham1412000@gmail.com>
2023-04-12 10:18:15 +0700

    fix: homepage login link

--- pages/index.jsx
+++ pages/index.jsx
@@ -60,7 +60,7 @@ const Home = ({ trendingPlaces, aroundPlaces, topRatingPlaces }) => {
             type="button"
             className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full"
           >
-            <Link href="/login">Login</Link>
+            <Link href="/api/auth/signin">Login</Link>
           </button>
         </div>
 
